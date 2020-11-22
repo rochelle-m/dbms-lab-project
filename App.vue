c82680748191ed5c01ee2fc73ae8de92f8394fc9
rochelle-m <mca.1930@unigoa.ac.in>
2020-11-23 00:29:27 +0530

    Add project files

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>{{ appTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appTitle: `📻 Music & Podcast streaming ♪`,
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  background: rgb(34, 33, 33);
+  color: white;
+  box-sizing: border-box;
+  font-family: "Josefin Sans", sans-serif;
+}
+
+h2 {
+  font-size: 36px;
+  text-align: center;
+  padding-bottom: 20px;
+  border-bottom: 1px solid silver;
+}
+</style>
