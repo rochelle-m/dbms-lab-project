4c47c4985c8653d4aa1943c5064751f5cfc58e9c
rochelle-m <mca.1930@unigoa.ac.in>
2020-11-28 22:08:36 +0530

    Add duplicate email validation

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -8,14 +8,32 @@ class User {
   }
 
   create(newUser, result) {
-    db.query("INSERT INTO users SET ?", newUser, (err, res) => {
-      if (err) {
-        result(err, null);
-        return;
+    // duplicate email validation
+    db.query(
+      `SELECT count(email) as num from users where email ='${newUser.email}'`,
+      (err, res) => {
+        if (err) {
+          result(err, null);
+          return;
+        }
+
+        const numOfEmailEntries = res[0].num;
+
+        if (numOfEmailEntries) {
+          result({ message: "Duplicate email" }, null);
+        } else {
+          // insert record into table
+          db.query("INSERT INTO users SET ?", newUser, (err, res) => {
+            if (err) {
+              result(err, null);
+              return;
+            }
+            console.log({ id: res.insertId, ...newUser });
+            result(null, { id: res.insertId, ...newUser });
+          });
+        }
       }
-      console.log({ id: res.insertId, ...newUser });
-      result(null, { id: res.insertId, ...newUser });
-    });
+    );
   }
 }
 
