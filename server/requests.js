1147c836ba8d85b2cf527a5a53daad4ed8260cb1
rochelle-m <mca.1930@unigoa.ac.in>
2020-12-01 01:56:17 +0530

    Create and populate genres table using spotify's endpoint

---
+++ server/requests.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const db = require("./db/db.js");
+const fetch = require("node-fetch");
+
+db.query(
+  `CREATE TABLE genres(idgenre int primary key AUTO_INCREMENT, name varchar(20))`,
+  (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(res);
+  }
+);
+
+(async function fetchGenres() {
+  const res = await fetch(
+    "https://api.spotify.com/v1/recommendations/available-genre-seeds",
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + process.env.ACCESS_TOKEN,
+      },
+    }
+  );
+  const data = await res.json();
+
+  data.genres.map((g) => {
+    db.query(`INSERT INTO genres (name) VALUES ('${g}')`, (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+    });
+  });
+})();
