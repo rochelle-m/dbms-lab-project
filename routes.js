547dcc28d390342d8b0408ae912531095af8f9bc
rochelle-m <mca.1930@unigoa.ac.in>
2020-12-06 22:16:08 +0530

    Add route for /home

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
+import Home from "./Home.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -7,6 +8,10 @@ export const router = createRouter({
     {
       path: "/",
       component: App,
+    },
+    {
+      path: "/home",
+      component: Home,
     },
   ],
 });
