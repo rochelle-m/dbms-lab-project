170c068a5cebf9c81e58977c79145a29321c163d
rochelle-m <mca.1930@unigoa.ac.in>
2020-12-09 16:58:01 +0530

    Add artists to db

---
+++ server/Requests/artistIds.js
@@ -0,0 +1,10 @@
+module.exports = [
+  "0du5cEVh5yTK9QJze8zA0C",
+  "60d24wfXkVzDSfLS6hyCjZ",
+  "3QLIkT4rD2FMusaqmkepbq",
+  "4Ti0EKl2PVEms2NRMVGqNe",
+  "6prmLEyn4LfHlD9NnXWlf7",
+  "5Pwc4xIPtQLFEnJriah9YJ",
+  "7b6Ui7JVaBDEfZB9k6nHL0",
+  "4npEfmQ6YuiwW1GpUmaq3F",
+];
