b5f249c5380fcd3a611b91a0598a8934748a0644
rochelle-m <mca.1930@unigoa.ac.in>
2020-12-12 19:21:34 +0530

    setup state management

--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import { router } from "./routes.js";
+import { store } from "./store/store.js";
 import MainApp from "./Main.vue";
 
 const app = createApp(MainApp);
 app.use(router);
+app.use(store);
 app.mount("#app");
