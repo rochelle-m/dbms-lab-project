147b55c78322b73c6f2cc30d9c9f0114a491707c
rochelle-m <mca.1930@unigoa.ac.in>
2020-12-15 10:27:48 +0530

    Add tracks

--- server/Requests/ids.tracks.js
+++ server/Requests/ids.tracks.js
@@ -1,1 +1,5 @@
-module.exports = [];
+module.exports = [
+  "3y2kWqVwRjwU49sbz5QGRD",
+  "0YUp47sg3umyQvNnXWfVbT",
+  "10rChmECwPcvTTj4w07hq4",
+];
---
+++ server/Requests/tracks.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const tracksIds = require("./ids.tracks.js");
+
+const db = require("../db/db.js");
+const fetch = require("node-fetch");
+
+let params = {
+  ids: tracksIds.join(","),
+};
+
+(async function () {
+  const res = await fetch(
+    "	https://api.spotify.com/v1/tracks?ids=" + params.ids,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + process.env.ACCESS_TOKEN,
+      },
+    }
+  );
+  const tracksData = await res.json();
+
+  const trackObjs = tracksData.tracks.map(
+    ({ id, name, explicit, href, album }) => ({
+      idtracks: id,
+      name,
+      explicit,
+      href,
+      album_id: album.id,
+    })
+  );
+
+  trackObjs.map((track) => {
+    db.query("INSERT INTO tracks SET ?", track, (err, res) => {
+      if (err) {
+        console.log(err.errno);
+        return;
+      }
+      console.log(res);
+    });
+  });
+})();
